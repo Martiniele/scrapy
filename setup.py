0877017ce11a5bdca64ec23e2a62426b697158fb
Martiniele <ielegeek@sina.cn>
2019-01-01 20:41:43 +0800

    delete some no use code

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from os.path import dirname, join
 from pkg_resources import parse_version
 from setuptools import setup, find_packages, __version__ as setuptools_version
-wxx
+
 
 with open(join(dirname(__file__), 'scrapy/VERSION'), 'rb') as f:
     version = f.read().decode('ascii').strip()
